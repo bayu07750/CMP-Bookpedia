d919586bb029c69904ce54ffc602f9e241b558d4
bayu07750 <bayupratama07750@gmail.com>
2024-11-30 17:12:13 +0700

    [Android] enabled edge to edge

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/MainActivity.kt
@@ -3,10 +3,14 @@ package com.plcoding.bookpedia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.enableSavedStateHandles
 import com.plcoding.bookpedia.app.App
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
